45f12729c14e48f4e05bc5855b5d26d6ea298e80
soumikdhar5 <soumikdhar5@gmail.com>
2025-01-07 17:34:20 +0530

    updated with generateClassName

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,21 @@ import React from 'react'
 import MarkettingApp from "./components/markettingApp";
 import Header from './components/Header';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider , createGenerateClassName } from '@material-ui/core';
 
 export default () => {
 
+    const generateClassName = createGenerateClassName({
+        productionPrefix:'co',
+    })
 
     return (<BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
         <div>
             <Header />
             <MarkettingApp />
         </div>
+        </StylesProvider>
     </BrowserRouter>)
 
 }
